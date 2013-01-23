56c8bc964773e4ec5e8df01b24a382f7403105c1
misaizdaleka <milos.boskovic@gmail.com>
2013-01-23 20:57:32 +0100

    edit_post.php

--- application/views/edit_post.php
+++ application/views/edit_post.php
@@ -1,9 +1,14 @@
 
-
-<?php if($success == 1) { ?>
+<!-- Ovo je loša praksa - da koristiš vitičaste zagrade u view-u 
+	Imaš drugačiju sintaksu koja ti zamenjuje vitičaste (ispravio sam):
+-->
+<?php if($success): ?>
 <div style="color:white; background: green;">This post has been updated!</div>
+<?php endif; ?>
 
-<?php } ?>
+<!-- Takođe, napisao si bio if ($success == 1), što je nepotrebno, 
+	jer ti if ($success) radi istu stvar. I to sam ispravio.
+-->
 
 <form action='<?=base_url()?>posts/editpost/<?=$post["postID"]?>' method='post'>
 	<p>Title: <input name='title' type='text' value='<?=$post['title']?>'/></p>
