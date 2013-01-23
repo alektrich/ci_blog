c0e6e91f92f8dd7d6accfe28f1e83dc470ed0913
Aleksandar <alektrich@gmail.com>
2013-01-23 08:59:53 +0100

    Update application/controllers/upload.php
    Konačno upload.php

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -12,7 +12,7 @@ class Upload extends CI_Controller {
 	}
 
 	public function do_upload() {
-		$data['upload_data']=NULL;
+		//$data['upload_data']=NULL;
 		$config['upload_path'] = './uploads';
 		$config['allowed_types'] = 'gif|jpg|png';
 		$config['max_size'] = '100';
@@ -22,8 +22,9 @@ class Upload extends CI_Controller {
 
 		if(!$this->upload->do_upload()) {
 			$error = array('error'=>$this->upload->display_errors());
+			$this->load->view('upload_form', $error);
 		} else {
-			$data['upload_data'] = $this->upload->data();
+			$data = array('upload_data' => $this->upload->data());
 			$this->load->view('upload_success', $data);
 		}
 	}
