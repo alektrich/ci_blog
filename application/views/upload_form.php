0c126f9f1ae5e9d0bdb34831b8511c35b0248836
misaizdaleka <milos.boskovic@gmail.com>
2013-01-23 20:55:19 +0100

    - upload_form.php

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -1,11 +1,6 @@
 <?php echo $error; ?>
 
 <?php echo form_open_multipart('upload/do_upload'); ?>
-<?php
-$data_form = array(
-	'name' => 'userfile'
-);
-echo form_upload($data_form);
-?><br />
+<?php echo form_upload(array('name' => 'userfile')); //metuo sam ti ovo u jedan red ?><br />
 <?php echo form_submit('', 'Upload'); ?>
 <?php echo form_close(); ?>
